275b9f37269cc5e9034384a5da9a6c2a48ba73aa
Nicole Chan <nictscnotes@gmail.com>
2026-05-09 11:09:02 +1000

    Setup logout workflow in Supabase

--- playpal/users/urls.py
+++ playpal/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet, KidsViewSet, SupabaseWebhookView
+from .views import CustomUserViewSet, KidsViewSet, SupabaseWebhookView, LogoutView
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet, basename='user')
@@ -9,4 +9,5 @@ router.register(r'kids', KidsViewSet, basename='kids')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/webhook/', SupabaseWebhookView.as_view()),
+    path('auth/logout/', LogoutView.as_view()),
 ]
