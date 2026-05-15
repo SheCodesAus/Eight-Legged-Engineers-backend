16d6fd93f9d17438d67733a2deef197e33e5eb9f
unknown <khoeff93@gmail.com>
2026-05-16 05:58:45 +1000

    updated local admin

--- playpal/venues/admin.py
+++ playpal/venues/admin.py
@@ -6,7 +6,7 @@ from .models import Venue
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
-    list_display = ('name', 'sub_category', 'suburb', 'main_category', 'is_published', 'is_archived', 'kids_eat_free')
+    list_display = ('name', 'sub_category', 'suburb', 'main_category', 'address', 'image_url', 'is_published', 'is_archived', 'kids_eat_free')
     list_filter = ('main_category', 'sub_category', 'is_published', 'is_archived', 'indoor_outdoor')
     search_fields = ('name', 'address', 'suburb', 'sub_category')
     list_editable = ('is_published', 'is_archived')
